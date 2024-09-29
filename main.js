866f0f9e82a950da3040f2f3059ab55b1a848a43
DanielSanm <danielelias3221@gmail.com>
2024-09-29 19:30:53 -0300

    finishing main window

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: `${__dirname}/renderer.js`,
+      nodeIntegration: true
+    }
   })
 
   win.loadFile('index.html')
@@ -11,4 +15,16 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
